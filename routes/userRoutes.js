71b5bd8d532831667b3b4534f67a00c70375b98e
kadijab <kadija_bell@yahoo.com>
2022-08-02 22:20:47 -0400

    re sturcture

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/userCtrl');
-const User = require('../../models/user');
+const User = require("../models/user")
 
-
-
-
-//router.post('/', userCtrl.createUser)
-
-//added for user login
 
 router.post('/login', userCtrl.login);
 router.post('/signup', userCtrl.signup)
 
-//the routes below are not the *focus* of the app, but for functionality
-//router.put('/:id', userCtrl.updateUser)
-
-//router.delete('/:id', userCtrl.deleteIt)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = 6001;
 const morgan = require('morgan');
+const cors = require('cors')
 
 app.get('/', function(req, res) {
     res.send("Connected to Express")
     console.log("Connected to Express")
 })
 
-const cors = require('cors')
 //require('dotenv').config();
 <<<<<<< HEAD
 require('./db/connection')
 =======
 require('./db/connection');
+<<<<<<< HEAD
+=======
+
+//middlewares
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use("/users/", require("./routes/userRoutes.js"));
+app.use("/progress/", require('./routes/progressRoutes.js'));
+
+
+app.get('/api', (req, res)=>{
+    res.json({message:"Connected to Express"})
+})
+
+app.listen(PORT, () => {
+    console.log(`✅ PORT: ${PORT} 🌟`);
+})
+>>>>>>> 684fd7b (re sturcture)
